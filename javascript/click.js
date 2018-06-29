12c1d5a8902c19383bdb5d9e055d95aa1c03ed7d
Gladys Monje Landeros <gmonjela@gmonjela-mn1.linkedin.biz>
2018-06-28 17:14:12 -0700

    everything

--- javascript/click.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-    let clicks = 0;
-    const div1 = document.getElementById('div1');
-    div1.addEventListener('click', function() {
-        clicks++;
-        div1.innerHTML = 'Clicked ' + clicks + ' times!';
-    });
-});
